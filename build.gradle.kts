88227964d0f2cb74c5b77bbd91ad97f14a114b2a
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-12-02 18:32:41 +0100

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 val junitJupiterVersion = "5.7.0"
-val ktorVersion = "1.4.1"
-val dusseldorfVersion = "1.4.1.82ec597"
+val dusseldorfVersion = "1.4.3.1978e78"
+val ktorVersion = ext.get("ktorVersion").toString()
+
 val jsonassertVersion = "1.5.0"
-val assertjVersion = "3.11.1"
+val assertjVersion = "3.18.1"
 
 val mainClass = "no.nav.omsorgspenger.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.20"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
@@ -19,7 +20,7 @@ java {
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/4754df622cd6609bd54f456136cb8aa43d2d9159/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/1978e782545516a275f8ff12690cfc07665c670d/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
@@ -78,6 +79,6 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "6.7"
+        gradleVersion = "6.7.1"
     }
 }
