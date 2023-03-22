9253f86f615ba0bfe815e82d6aa5faf697a9193a
albrektsson <emil.albrektsson@nav.no>
2023-03-22 08:28:07 +0100

    Fjernet jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,6 @@ repositories {
             password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
         }
     }
-    maven("https://jitpack.io")
     mavenCentral()
 }
 
