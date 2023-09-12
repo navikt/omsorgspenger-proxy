741157a62830d9bccc63f75f031ad01c2dd3d0ca
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2023-09-12 09:12:09 +0200

    Update Config.kt

--- src/main/kotlin/no/nav/omsorgspenger/config/Config.kt
+++ src/main/kotlin/no/nav/omsorgspenger/config/Config.kt
@@ -11,10 +11,6 @@ internal object Config {
         internal val url = env.getOrFail("K9_SAK_BASE_URL")
     }
 
-    internal class Sak(env: Map<String, String>) {
-        internal val url = env.getOrFail("SAK_BASE_URL")
-    }
-
     internal class STS(env: Map<String, String>) {
         internal val url = env.getOrFail("STS_TOKEN_URL")
     }
